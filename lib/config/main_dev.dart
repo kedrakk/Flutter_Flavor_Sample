489adceebcc7b07c119e75fa36f0e98305ebe46a
kedrakk <keaindrak@gmail.com>
2023-06-21 10:37:07 +0630

    IOS firebase

--- lib/config/main_dev.dart
+++ lib/config/main_dev.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flavor_sample/config/appconfig.dart';
 import 'package:flutter_flavor_sample/main.dart';
 
+import '../firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   AppConfig appConfig = AppConfig(
     appName: "Flavor DEV",
     appThemeData: ThemeData(
--- lib/config/main_prod.dart
+++ lib/config/main_prod.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flavor_sample/config/appconfig.dart';
+import 'package:flutter_flavor_sample/firebase_options.dart';
 import 'package:flutter_flavor_sample/main.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   AppConfig appConfig = AppConfig(
     appName: "Flavor PROD",
     appThemeData: ThemeData(
